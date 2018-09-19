c6d844cf011f1447d2051a5c322cb251a130ccfb
mich <mdelcarpio@gmail.com>
2018-09-19 13:34:01 -0300

    Funcionamento

--- src/App.js
+++ src/App.js
@@ -8,24 +8,23 @@ class App extends Component {
       this.state = {
         listaDeCompras : listaDeItens
       }
-      this.onAdicionarItem.bind(this.onAdicionarItem);
-      this.onRemoverItem.bind(this.onRemoverItem);
+      this.onAdicionarItem = this.onAdicionarItem.bind(this);
+      this.onRemoverItem = this.onRemoverItem.bind(this);
   }
   onAdicionarItem (elemento){
-    console.log(elemento);
-    let lista = this.state.listaDeCompras;
-    lista.push(elemento);
-    this.setState({listaDeCompras : lista});
+    const lista = [...this.state.listaDeCompras,elemento];
+    this.setState({listaDeCompras:lista});
   }
   onRemoverItem(elemento){
-    console.log(elemento);
+    const lista = this.state.listaDeCompras.filter(item => item  != elemento);
+    this.setState({listaDeCompras:lista});
   }
   
   render() {
     const {listaDeCompras} = this.state;
     return (
       <div>      
-        <FormularioAdicionarItem onAdicionarItem={this.onAdicionarItem}/>        
+        <FormularioAdicionarItem onAdicionarItem={this.onAdicionarItem}/>
         <TabelaDeItens itens={listaDeCompras} onRemoverItem = {this.onRemoverItem}/>
       </div>
       );
--- src/ListaDeCompras/TabelaDeComprasv3.js
+++ src/ListaDeCompras/TabelaDeComprasv3.js
@@ -2,28 +2,40 @@ import React, { Component } from 'react';
 import {Table,Button,Form, Grid, Icon} from 'semantic-ui-react';
 class FormularioAdicionarItem extends Component{
     state = {item:''}
-    
-    
-    render(){
+    onChange = (e, {name,value}) => this.setState({[name]:value})
+    constructor(props){
+        super(props);
+    }
+    enviar = () => {
         const {onAdicionarItem}= this.props;
         const {item} = this.state;
-        return (
-            <Grid>
-                <Grid.Row>
+        onAdicionarItem(item);
+        this.setState({item: ''});
+    }
+    
+    render(){
+        const {item} = this.state;
+        return <Grid>
+            <Grid.Row>
                 <Grid.Column width={6}>
-                    <Form size={'large'} onSubmit={()=>onAdicionarItem(item)}>
-                        <Form.Input placeholder='Que você vai comprar hoje?' name='item' value={item}/>
-                        <Form.Button type='submit'>Adicionar</Form.Button>
-                        
-                    </Form>
-                </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        )
+                    <Form size={'large'} onSubmit={this.enviar }>
+                    <Form.Input label={'Item'} placeholder='Que você vai comprar hoje?'
+                                onChange={this.onChange}
+                                name='item'
+                                value={item}/>
+                    <Form.Button fluid
+                                 type='submit'
+                                 disabled={!this.state.item}>
+                        Adicionar
+                    </Form.Button>
+
+                </Form>
+            </Grid.Column>
+        </Grid.Row> </Grid>
     }
 }
 const BotaoRemover = (onRemoverItem) => {
-        return (
+    return (
         <Button onClick={onRemoverItem} icon><Icon name="trash alternate"></Icon></Button>
         )
 }
@@ -32,41 +44,46 @@ const BotaoRemover = (onRemoverItem) => {
 class TabelaDeItens extends Component {
     constructor(props){
         super(props);
-        this.state = {itens:props.itens}
-        this.corpoDaTabela.bind(this.corpoDaTabela);
+        //this.state = {itens:props.itens}
+        this.corpoDaTabela = this.corpoDaTabela.bind(this);
     }
 
     corpoDaTabela = () => {
-        const {itens} = this.state;
-        const {onRemoverItem} = this.props;
+        const {onRemoverItem,itens} = this.props;
         return itens.map( 
                         item => <Table.Row key={item}>
                                 <Table.Cell>{item}</Table.Cell>                                            
-                                <Table.Cell><button
+                                <Table.Cell><Button
                 onClick={() => onRemoverItem(item)}
                 type="button"
-              >Remover</button></Table.Cell>
+              >Remover</Button></Table.Cell>
                                 </Table.Row>
         )
         }
-        
+    listaVazia = () =>{
+        return (
+            <div>A lista de Compras está Vazia</div>
+        )
+    }
 
     
     render () {
-        
-        return (
-        <Table striped basic='very'>
-            <Table.Header>
-                <Table.Row>                
-                <Table.HeaderCell>Nome</Table.HeaderCell>
-                <Table.HeaderCell>Operação</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                <this.corpoDaTabela/>
-            </Table.Body>
-        </Table>
-        )
+            if (this.props.itens.length < 1) {
+               return <this.listaVazia/>
+            }else {
+               return  <Table striped basic='very'>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Nome</Table.HeaderCell>
+                            <Table.HeaderCell>Operação</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        <this.corpoDaTabela/>
+                    </Table.Body>
+                </Table>
+            }
+
     }
 }
 export {TabelaDeItens,FormularioAdicionarItem}
