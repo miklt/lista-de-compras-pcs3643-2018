8b27303f2f82ec81a91061f49c79336a80fcecd0
michelet <michelet@usp.br>
2018-09-20 15:11:10 -0300

    Versão 1.0

---
+++ src/ListaDeCompras/TabelaDeItens/TabelaDeItensv3.js
@@ -0,0 +1,35 @@
+import {Component} from "react";
+import {Grid, Icon,Label, Message} from "semantic-ui-react";
+import React from "react";
+import {BotaoRemover} from '../Botoes/index'
+
+class TabelaDeItens extends Component {
+    constructor(props){
+        super(props);
+        this.state = {itens:props.itens};
+        this.corpoDaTabela = this.corpoDaTabela.bind(this);
+    }
+    corpoDaTabela = () => {
+        const {onRemoverItem,itens} = this.props;
+        return  <div>
+            {itens.map(item =>
+                <Label key={item} color={'blue'} size={'small'}>{item}<BotaoRemover onClick={() => onRemoverItem(item)}/></Label>
+
+            )}
+        </div>
+    };
+    listaVazia = () =>{
+        return (
+            <Message warning attached={'bottom'}><Icon name={'warning'}/>A lista de Compras está Vazia</Message>
+        )
+    };
+
+    render () {
+        if (this.props.itens.length < 1) {
+            return <this.listaVazia/>
+        }else {
+            return  <this.corpoDaTabela/>
+        }
+    }
+}
+export default TabelaDeItens
